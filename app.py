a99410157c31a7838c95d75e8b2640657799c73f
Palbha Kulkarni (Nazwale) <palbhanazwale@gmail.com>
2025-05-20 17:42:58 -0400

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def get_response(message, chat_history, model_choice, api_key):
 
     chat_history.append({"role":"assistant","content": reply})
     print("Chat",chat_history)
-    return chat_history
+    return reply,chat_history
 # Gradio app
 with gr.Blocks(css=custom_css) as app:
     with gr.Row():
